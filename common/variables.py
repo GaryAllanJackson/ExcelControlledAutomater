933f19b34b8f252758d9371bf47e090f552ae8b0
GaryAllanJackson <gary.a.jackson@gmail.com>
2025-05-07 12:22:53 -0400

    Updated variables removing specific PC paths

--- common/variables.py
+++ common/variables.py
@@ -1,7 +1,7 @@
-command_file_name = r"C:\Gary\Python_Development\Excel_Controlled_Automater\data\CommandScripts.xlsx"
+command_file_name = r"data\CommandScripts.xlsx"
 command_sheet_name = "CommandScripts"
 
-log_file_name = r"C:\Gary\Python_Development\Excel_Controlled_Automater\data\TestLogs.xlsx"
+log_file_name = r"data\TestLogs.xlsx"
 log_sheet_name = "Logs"
 
 get_current_url_in_action_method = "current_url"
